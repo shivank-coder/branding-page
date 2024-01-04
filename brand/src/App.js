a30ab6856ebb708ce38235fadfd9f78e7a7b8333
Mayank Singh <amanjh05@gmail.com>
2024-01-04 12:30:02 +0530

    Your commit message

---
+++ brand/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Navigation />
+
+    </div>
+    
+  );
+}
+
+export default App;
